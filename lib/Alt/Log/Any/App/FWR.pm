c11545be4113319c0a736a334f44c87d7400d9c2
Steven Haryanto <stevenharyanto@gmail.com>
2012-12-28 03:32:39 +0000

    Import of SHARYANTO/Alt-Log-Any-App-FWR-0.01 from CPAN.
    gitpan-cpan-distribution: Alt-Log-Any-App-FWR
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Alt-Log-Any-App-FWR-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Alt/Log/Any/App/FWR.pm
@@ -0,0 +1,32 @@
+package Alt::Log::Any::App::FWR;
+use base qw(Alt::Base);
+
+our $VERSION = '0.01'; # VERSION
+
+1;
+# ABSTRACT: An easy way to use Log::Any in applications
+
+__END__
+=pod
+
+=head1 NAME
+
+Alt::Log::Any::App::FWR - An easy way to use Log::Any in applications
+
+=head1 VERSION
+
+version 0.01
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
